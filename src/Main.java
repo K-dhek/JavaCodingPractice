c7ca11848522f6070e8c57963e39340aa61df158
Krishna Dhek <krishnadhek98@gmail.com>
2023-06-11 22:22:03 +0530

    added a basic method to print upto n numbers

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is my second commit: Updated git ignore");
+        printNumbers(10);
     }
+
+    //print up to n numbers
+    public static void printNumbers(int n){
+
+        if(n>=0){
+            for (int i = 0; i < n; i++) {
+                System.out.println(i);
+
+            }
+        }
+    }
+
 }
