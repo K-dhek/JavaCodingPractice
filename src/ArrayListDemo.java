39b39f41123180e8b4d0c3c2c310e4c0215abf45
Krishna Dhek <krishnadhek98@gmail.com>
2023-06-12 00:24:23 +0530

    working of some methods

--- src/ArrayListDemo.java
+++ src/ArrayListDemo.java
@@ -3,17 +3,22 @@ import java.util.ArrayList;
 public class ArrayListDemo {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
+        System.out.println(list.isEmpty());
         list.add(10);
         list.add(12);
         list.add(13);
         list.add(14);
         list.add(15);
 
-        if(list.contains(10)){
-            for (int i : list) {
-                System.out.println(i);
 
+            for (Integer i : list) {
+                System.out.println(i);
             }
-        }
+
+        System.out.println(list.size());
+        System.out.println(list.indexOf(13));
+        System.out.println(list.remove(1));
+        System.out.println(list);
+
     }
 }
